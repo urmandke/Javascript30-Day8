0057d7e180c92e1a16d0f31f37e574095a58d185
Udayraj <udayraj.m@bizamica.com>
2021-08-10 19:48:59 +0530

    Feat:Detect mousedown event in draw

---
+++ script.js
@@ -0,0 +1,23 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = '#BADASS';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e){
+   if(!isDrawing) return; // stop for running when they are not moused down
+   console.log(e);
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup',() => isDrawing = false);
+canvas.addEventListener('mousseout',() => isDrawing = false);
