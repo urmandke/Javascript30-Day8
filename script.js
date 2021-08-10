4ea5ce1c9ad60344a4072baed92fce97f1129094
Udayraj <udayraj.m@bizamica.com>
2021-08-10 20:01:01 +0530

    Feat:Update draw to draw a line of width 100 and start at where mouse is clicked down

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADASS';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 100;
 
 let isDrawing = false;
 let lastX = 0;
@@ -15,9 +16,18 @@ let lastY = 0;
 function draw(e){
    if(!isDrawing) return; // stop for running when they are not moused down
    console.log(e);
+   ctx.beginPath()
+   ctx.moveTo(lastX,lastY);
+   ctx.lineTo(e.offsetX,e.offsetY);
+   ctx.stroke();
+   lastX = e.offsetX;
+   lastY = e.offsetY;
 }
 
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mousedown', (e) => {
+    [lastX , lastY] = [e.offsetX , e.offsetY];
+    isDrawing = true;
+});
 canvas.addEventListener('mouseup',() => isDrawing = false);
 canvas.addEventListener('mousseout',() => isDrawing = false);
